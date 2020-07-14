6c14de06ddb5e1ab9214b4835d603a3d227978f4
aapetsi <apetsiampiah@gmail.com>
2020-07-14 20:28:50 +0000

    add styling files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import UrlList from './UrlList'
 import UrlForm from './UrlForm'
 
 const App = () => {
+  const [urls, setUrls] = useState([])
+
+  useEffect(() => {}, [urls])
   return (
     <div>
       <h1>URL Shortener</h1>
       <UrlForm />
-      <UrlList />
+      <UrlList urls={urls} />
     </div>
   )
 }
--- client/src/components/UrlList.js
+++ client/src/components/UrlList.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Url from './Url'
 
-const UrlList = () => {
+const UrlList = (props) => {
+  const { urls } = props
   return (
     <div>
+      {urls.map((url) => (
+        <Url key={url.id} shortUrl={url.shortUrl} />
+      ))}
       <Url />
     </div>
   )
