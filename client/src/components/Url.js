bb8050f45c78a914ba909aba16b4a0cb358b0460
aapetsi <apetsiampiah@gmail.com>
2020-07-14 09:30:52 +0000

    add url list component

---
+++ client/src/components/Url.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Url = () => {
+  return (
+    <div>
+      <p>Single url</p>
+    </div>
+  )
+}
+
+export default Url
---
+++ client/src/components/UrlList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UrlList = () => {
+  return (
+    <div>
+      <p>all urls</p>
+    </div>
+  )
+}
+
+export default UrlList
