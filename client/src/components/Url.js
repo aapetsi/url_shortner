9aab67ec665b7115f018f83efa315a742cb3a9d7
aapetsi <apetsiampiah@gmail.com>
2020-07-15 21:17:24 +0000

    delete unused code

--- client/src/components/Url.js
+++ client/src/components/Url.js
@@ -26,6 +26,3 @@ const Url = (props) => {
 }
 
 export default Url
-
-// const navigateToExternalUrl = (url: string, shouldOpenNewTab: boolean = true) =>
-//   shouldOpenNewTab ? window.open(url, '_blank') : (window.location.href = url)
