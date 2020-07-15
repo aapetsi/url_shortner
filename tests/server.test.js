dcb01a10f22a905b5088b29e690b811abb651136
aapetsi <apetsiampiah@gmail.com>
2020-07-15 10:29:39 +0000

    begin add tests for backend

---
+++ tests/server.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest')
+const server = require('../server')
+const mongoose = require('mongoose')
+
+describe('Index route', () => {
+  test('should run', async () => {
+    expect(1).toBe(1)
+  })
+})
