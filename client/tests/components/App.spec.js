6a07da2649ef1a8399772249e3ef6c453b1f7e17
aapetsi <apetsiampiah@gmail.com>
2020-07-17 06:27:34 +0000

    begin testing of components

---
+++ client/tests/components/App.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import App from '../../src/components/App'
+
+describe('Test cases for App.js', () => {
+  test('should evaluate 1 == 1', () => {
+    expect(1).toBe(1)
+  })
+})
---
+++ client/tests/components/Url.spec.js
@@ -0,0 +1,2 @@
+import React from 'react'
+import Url from '../../src/components/Url'
