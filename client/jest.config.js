ec72c707eee9bb5126ef062223d463ceffb8b1d1
aapetsi <apetsiampiah@gmail.com>
2020-07-18 14:54:39 +0000

    modify package.json file

--- client/jest.config.js
+++ client/jest.config.js
@@ -23,4 +23,6 @@ module.exports = {
 
   // Module file extensions for importing
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  transformIgnorePatterns: ['<rootDir>/src/styles'],
 }
