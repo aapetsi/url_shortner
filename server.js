f90df91da3cc641397ced85ad02e1efa644d24d2
aapetsi <apetsiampiah@gmail.com>
2020-07-20 19:08:48 +0000

    serve react client from front end

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const connectDB = require('./config/db')
 const urlRouter = require('./routes/api/url')
+const buildPath = path.join(__dirname, 'client', 'build')
 
 const app = express()
 
@@ -11,11 +13,21 @@ connectDB()
 // app middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(buildPath))
 
 // cors middleware
 app.use(cors())
 
 // Routes middleware
 app.use('/api/url', urlRouter)
+
+// send frontend react app
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
 
 module.exports = app
