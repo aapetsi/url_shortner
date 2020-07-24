4e512483f0b342709b71db02d67fb1979e387ffb
aapetsi <apetsiampiah@gmail.com>
2020-07-24 12:20:26 +0000

    test out more typescript stuff

---
+++ server/src/controllers/UrlController.ts
@@ -0,0 +1,8 @@
+import {Request, Response} from 'express'
+import {Controller, Get, Post, Middleware} from '@overnightjs/core'
+import {Logger} from '@overnightjs/logger'
+
+@Controller('api')
+export class UrlController {
+  
+}
