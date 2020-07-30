816a6fab2144f22ebc7526ea51670c8b298b9701
aapetsi <apetsiampiah@gmail.com>
2020-07-30 02:49:43 +0000

    add check for existing user

--- server/src/controllers/url/user.controller.ts
+++ server/src/controllers/url/user.controller.ts
@@ -1,8 +1,16 @@
 import {Request, Response} from 'express'
 import User from '../../models/User.model'
 
-const registerUser = async (req: Request, res: Response) : Promise<void> => {
-  res.send('regiser')
+const registerUser = async (req: Request, res: Response) => {
+  const {email} = req.params
+  try {
+    const foundUser = User.findOne({email})
+    if (foundUser) {
+      return res.status(400).json({message: 'User already exists'})
+    }
+  } catch (error) {
+      return res.status(500).json({message: error.message})
+  }
 }
 
 const loginUser = async (req: Request, res: Response) : Promise<void> => {
