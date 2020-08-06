d8b2c2d133d35ebe387894578107643c9813b3f4
aapetsi <apetsiampiah@gmail.com>
2020-08-06 22:53:32 +0000

    still errors

---
+++ vue-client/src/components/tests/UrlForm.spec.ts
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import UrlForm from '../UrlForm.vue'
+
+describe('UrlForm Component', () => {
+  test('should render form', () => {
+    const wrapper = shallowMount(UrlForm)
+    expect(1).toBe(1)
+  })
+})
