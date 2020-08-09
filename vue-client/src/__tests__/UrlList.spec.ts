8b0e7536378ae64f6d69df49156a0b1eb35e8a4b
aapetsi <apetsiampiah@gmail.com>
2020-08-09 11:08:23 +0000

    complete tests on url list component

--- vue-client/src/__tests__/UrlList.spec.ts
+++ vue-client/src/__tests__/UrlList.spec.ts
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import { UrlList } from '../components'
 
 describe('UrlList.vue component', () => {
-  test('should render Urls list', async () => {
+  test('should render no Urls list', async () => {
     const handleDelete = jest.fn()
     const wrapper = shallowMount(UrlList, {
       propsData: {
@@ -11,6 +11,51 @@ describe('UrlList.vue component', () => {
         handleDelete
       }
     })
+    const urlList = wrapper.find('tbody')
+    expect(urlList.element.children).toHaveLength(0)
   })
   
+  test('should render 5 urls', async () => {
+    const handleDelete = jest.fn()
+    const wrapper = shallowMount(UrlList, {
+      propsData: {
+        urls: [
+          {
+            _id: 'asdf',
+            shortUrl: '',
+            originalUrl: '',
+            shortUrlHash: '',
+          },
+          {
+            _id: 'as',
+            shortUrl: '',
+            originalUrl: '',
+            shortUrlHash: '',
+          },
+          {
+            _id: 'd',
+            shortUrl: '',
+            originalUrl: '',
+            shortUrlHash: '',
+          },
+          {
+            _id: 'dd',
+            shortUrl: '',
+            originalUrl: '',
+            shortUrlHash: '',
+          },
+          {
+            _id: 'fa',
+            shortUrl: '',
+            originalUrl: '',
+            shortUrlHash: '',
+          }
+        ],
+        error: '',
+        handleDelete
+      }
+    })
+    const urlList = wrapper.find('tbody')
+    expect(urlList.element.children).toHaveLength(5)
+  })
 })
