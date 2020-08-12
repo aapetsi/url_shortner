9697c105919d39ed5c88e1f780bd2bab16b6090d
aapetsi <apetsiampiah@gmail.com>
2020-08-12 19:26:51 +0000

    add authentication middleware

--- server/src/controllers/auth/auth.controller.ts
+++ server/src/controllers/auth/auth.controller.ts
@@ -5,6 +5,7 @@ export const login = async (req: Request, res: Response) => {
   const {email, password} = req.body
   try {
     const user = await User.findOne({email})
+    // implement user authentication here
     res.send(user)
   } catch (error) {
     return res.status(500).json({message: error.message})
