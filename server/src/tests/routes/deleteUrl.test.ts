cd34d79fdb9cc2a930e1d1785f695dc3fa69c974
aapetsi <apetsiampiah@gmail.com>
2020-08-21 15:08:50 +0000

    complete tests refactor for delete one route

--- server/src/tests/routes/deleteUrl.test.ts
+++ server/src/tests/routes/deleteUrl.test.ts
@@ -8,6 +8,14 @@ let id : string
 let token: ITokenData
 const registerApi = '/api/auth/register'
 
+const createResponse = async (id: string, token: ITokenData) => {
+  const response = await request(server)
+    .delete(`/api/url/one/${id}`)
+    .set('Authorization', token.token)
+  
+  return response
+}
+
 beforeAll(async () => {
   try {
     await clearDB()
@@ -21,7 +29,8 @@ beforeAll(async () => {
     const savedUrl = await Url.create({
       shortUrlHash: '2nf8dw8a',
       shortUrl: 'https://pbid.io/2nf8dw8a',
-      originalUrl: 'https://yahoo.com'
+      originalUrl: 'https://yahoo.com',
+      dateCreated: '2020-08-20T10:58:01.964Z'
     })
     id = savedUrl._id
 
@@ -34,23 +43,21 @@ beforeAll(async () => {
 
 describe('Test delete a url from database', () => {
   test('should successfully delete url', async () => {
-    const res = await request(server).delete(`/api/url/one/${id}`).set('Authorization', token.token)
+    const res = await createResponse(id, token) 
 
     expect(res.status).toBe(200)
     expect(res.body.message).toBe('Url has been deleted')
   })
 
   test('should return url not found', async () => {
-    const res = await request(server).delete('/api/url/one/5f0b3a60bcf44a1b50d309cc').set('Authorization', token.token)
+    const res = await createResponse('5f0b3a60bcf44a1b50d309cc', token)
 
     expect(res.status).toBe(404)
     expect(res.body.message).toBe('No such url exists')
   })
 
   test('should case a server error with wrong id', async () => {
-    const res = await request(server)
-      .delete('/api/url/one/someid')
-      .set('Authorization', token.token)
+    const res = await createResponse('someid', token)
 
     expect(res.status).toBe(500)
     expect(res.body.message).toBeDefined()
