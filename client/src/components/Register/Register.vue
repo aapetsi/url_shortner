b91ca8d9e1a528b785879b85a392380e2eb6a6db
aapetsi <apetsiampiah@gmail.com>
2020-08-22 22:18:17 +0000

    setup user registration route

--- client/src/components/Register/Register.vue
+++ client/src/components/Register/Register.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <form>
-      <input type="text" required placeholder="username">
-      <input type="email" name="email" required placeholder="email">
-      <input type="password" required placeholder="Password">
-      <input type="password" required placeholder="Confirm Password">
+    <form v-on:submit="handleRegister">
+      <input v-model="username" type="text" required placeholder="username">
+      <input v-model="email" type="email" name="email" required placeholder="email">
+      <input v-model="password" type="password" required placeholder="Password">
+      <input v-model="password2" type="password" required placeholder="Confirm Password">
       <button type="submit">Register</button>
     </form>
   </div>
@@ -12,14 +12,29 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import AxiosNoAuth from '@/utils/AxiosNoAuth'
+
 export default Vue.extend({
   name: 'Register' as string,
   data: () => ({
-    username: '',
-    email: '',
-    password: '',
-    password2: ''
-  })
+    username: 'johndoe',
+    email: 'johndoe@gmail.com',
+    password: '123456',
+    password2: '123456'
+  }),
+  methods: {
+    async handleRegister(event: Event) {
+      event.preventDefault()
+      const res = await AxiosNoAuth().post('/auth/register', {
+        username: this.username, 
+        email: this.email, 
+        password: this.password, 
+        password2: this.password2
+      })
+
+      console.log(res.data)
+    }
+  }
 })
 </script>
 
