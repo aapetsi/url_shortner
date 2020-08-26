b1e573b9adcc2874b59af07b9a88fcc3565e9ff5
aapetsi <apetsiampiah@gmail.com>
2020-08-26 10:38:16 +0000

    begin converting to vuex

---
+++ client/src/store/modules/urls.ts
@@ -0,0 +1,21 @@
+// initial state
+const state = () => ({
+  urls: []
+})
+
+// getters
+const getters = {}
+
+// actions
+const actions = {
+  getUrls({ commit }) {
+
+  }
+}
+
+// mutations
+const mutations = {
+  setUrls(state, urls) {
+    
+  }
+}
