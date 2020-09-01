3416760c14e72115600573e7111d73b0a6223b83
aapetsi <apetsiampiah@gmail.com>
2020-09-01 17:20:28 +0000

    wip

--- client/src/__tests__/userActions.spec.ts
+++ client/src/__tests__/userActions.spec.ts
@@ -1,5 +1,22 @@
 import { actions } from '../store/modules/user'
-import { ActionContext } from 'vuex'
+import axios from 'axios'
+
+let url = ''
+let body = {}
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+
+// jest.mock('axios', () => ({
+//   post: (_url, _body) => {
+//     return new Promise((resolve) => {
+//       url = _url
+//       body = _body
+//       resolve(true)
+//     })
+//   },
+// }))
 
 describe('Test user actions', () => {
   test('should logout user', async () => {
@@ -8,7 +25,18 @@ describe('Test user actions', () => {
 
     expect(commit).toHaveBeenCalledWith('setUserLoggedIn', false)
     expect(commit).toHaveBeenCalledTimes(3)
-    expect(commit).toHaveBeenCalledWith('setUser', {user: {}, token: {}})
+    expect(commit).toHaveBeenCalledWith('setUser', { user: {}, token: {} })
     expect(commit).toHaveBeenCalledWith('clearErrors')
+  })
+
+  test('should register a user', async () => {
+    const commit = jest.fn()
+    const router = jest.fn()
+    mockedAxios.get.mockResolvedValueOnce({
+      data: {
+
+      }
+    })
+    await actions.login({ commit }, {email: 'johndoe', password: '123456', router: jest.fn()})
   })
 })
